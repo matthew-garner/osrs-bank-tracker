82cbce7bbc5569c1f76b3f31281d8856faab506e
matt <matthewgarner572@gmail.com>
2025-01-26 16:52:10 +0000

    reorganised folders

---
+++ src/main/java/catcat572/ExamplePlugin.java
@@ -0,0 +1,95 @@
+package catcat572;
+
+import com.google.inject.Provides;
+import javax.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.*;
+import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.events.ItemContainerChanged;
+import net.runelite.api.events.PostItemComposition;
+import net.runelite.client.config.ConfigManager;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.game.ItemManager;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Objects;
+
+
+@Slf4j
+@PluginDescriptor(
+	name = "Shows the value of bank over time"
+)
+public class ExamplePlugin extends Plugin
+{
+	@Inject
+	private Client client;
+
+	@Inject
+	ItemManager itemManager;
+
+	@Override
+	protected void startUp() throws Exception
+	{
+		initailiseCsv();
+	}
+
+	@Override
+	protected void shutDown() throws Exception
+	{
+		log.info("Example stopped!");
+	}
+
+	//bank id: 95
+	//inventory id: 93
+	//equipment id: 94
+	@Subscribe
+	public void onItemContainerChanged(ItemContainerChanged event)
+	{
+		ItemContainer container = event.getItemContainer();
+		if (container != null)
+		{
+			for (Item item : container.getItems())
+			{
+				String itemName = itemManager.getItemComposition(item.getId()).getName();
+				if (itemManager.getItemComposition(item.getId()).getName().equals("Coins"))
+				{
+					writeToCsv(item.getId(), itemName, item.getQuantity());
+				}
+				//log.info("Item id: {}", item);
+				if (item.getId() != -1 && itemManager.getItemComposition(item.getId()).isTradeable())
+				{
+					if (!Objects.equals(itemName, "Bank filler")){
+						//int itemPrice = itemManager.getItemPrice(item.getId()); //high alch price
+						log.info("Item ID: {}, Item name: {}, Item quantity {}", item.getId(), itemName, item.getQuantity());
+						writeToCsv(item.getId(), itemName, item.getQuantity());
+					}
+				}
+			}
+		}
+	}
+
+	private void initailiseCsv() {
+		String csvFilePath = "output.csv";
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(csvFilePath))) {
+			writer.write("Item ID,Item Name,Item Quantity");
+			writer.newLine();
+		} catch (IOException e) {
+			log.error("Failed to write to CSV file", e);
+		}
+	}
+
+	private void writeToCsv(int itemId, String itemName, int itemQuantity)
+	{
+		String csvFilePath = "output.csv";
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(csvFilePath, true))) {
+			writer.write(String.format("%d,%s,%d%n", itemId, itemName, itemQuantity));
+		} catch (IOException e) {
+			log.error("Failed to write to CSV file", e);
+		}
+	}
+
+}
---
+++ src/test/java/catcat572/PluginLauncher.java
@@ -0,0 +1,13 @@
+package catcat572;
+
+import net.runelite.client.RuneLite;
+import net.runelite.client.externalplugins.ExternalPluginManager;
+
+public class PluginLauncher
+{
+	public static void main(String[] args) throws Exception
+	{
+		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		RuneLite.main(args);
+	}
+}
